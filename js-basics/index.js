f4e6e3cfaca92746c1ec000dffa8d57c9ef60adb
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-04 16:41:37 +0100

    Arrays

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,20 +1,3 @@
-console.log(name);
-
-let firstName = 'Vladimir';
-let lastName = 'Krasko';
-
-const interestRate = 0.3;
-
-let isApproved = true;
-let middleName;
-let surname = null;
-
-let person = {
-    name: 'Vlada',
-    age: 32
-};
-
-person.name = 'John';
-person['name'] = 'Mary';
-
-console.log(person.name);
+let selectedColors = ['blue', 'red'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
