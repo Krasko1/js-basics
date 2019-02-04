f6d1a10dd78ed65179a43afd9f002761648719fd
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-04 20:55:38 +0100

    Types of functions

--- js-basics/index.js
+++ js-basics/index.js
@@ -2,4 +2,8 @@ function greet(name, lastName){
     console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('John', 'Jonic');
+function square(number){
+    return number * number;
+}
+
+console.log(square(2));
