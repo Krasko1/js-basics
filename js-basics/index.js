da0c1f96cb40edb3bbae44000454ef04c184b2d8
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-04 20:50:35 +0100

    Functions

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,3 +1,5 @@
-let selectedColors = ['blue', 'red'];
-selectedColors[2] = 1;
-console.log(selectedColors.length);
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Jonic');
