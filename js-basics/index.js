a0dc18c5cca7913615303fca4446b315231e522b
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-05 09:47:02 +0100

    If...else

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,9 +1,8 @@
-let a = 'red';
-let b = 'blue';
+let hour = 10;
 
-let c = a;
-a = b;
-b = c;
-
-console.log(a);
-console.log(b);
+if(hour >= 6 && hour < 12)
+    console.log('Good morning');
+else if(hour >= 12 && hour < 18)
+    console.log('Good afternoon');
+else
+    console.log('Good evening');
