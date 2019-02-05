33b2929c3835147e75f61b9c5e0148c5b3cdb335
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-05 10:24:58 +0100

    For...of

--- js-basics/index.js
+++ js-basics/index.js
@@ -8,5 +8,5 @@ for(let key in person)
 
 const colors = ['red', 'green', 'blue'];
 
-for(let index in colors)
-    console.log(index, colors[index]);
+for(let color of colors)
+    console.log(color);
