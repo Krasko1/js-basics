6632caf7fedaaf2bc119bcaeb138188392c11ea9
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-05 09:54:20 +0100

    Switch...case

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,8 +1,12 @@
-let hour = 10;
+let role = 'guest';
 
-if(hour >= 6 && hour < 12)
-    console.log('Good morning');
-else if(hour >= 12 && hour < 18)
-    console.log('Good afternoon');
-else
-    console.log('Good evening');
+switch(role){
+    case 'guest':
+        console.log('Guest user');
+        break;
+    case 'moderator':
+        console.log('Moderator user');
+        break;
+    default:
+        console.log('Unknown user');
+}
