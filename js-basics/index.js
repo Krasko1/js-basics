fcc905bbcd90cad39f97690649fbdb282beb507d
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-05 12:09:40 +0100

    Even and odd numbers

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,19 +1,8 @@
-checkSpeed(50);
+showNumbers(10);
 
-function checkSpeed(speed){
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-
-    if(speed < speedLimit + kmPerPoint){
-        console.log('Ok');
-        return;
+function showNumbers(limit){
+    for(let i = 0; i <= limit; i++){
+        const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+        console.log(i, message);
     }
-
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-
-    if(points >= 12)
-        console.log('License suspended');
-    else
-        console.log('Points', points);
 }
-
