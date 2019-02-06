21bb40f6e534927f29996fd5009f0cbf16b6fb37
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-06 23:01:04 +0100

    Objects-Ex.4 blog post object

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,23 +1,13 @@
-const address1 = new Address('a','b','c');
-const address2 = new Address('a','b','c');
-const address3 = address1;
+const post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 1,
+    comments:[
+        { author: 'd', body: 'e'},
+        { author: 'f', body: 'g'}
+    ],
+    isLive: true
+};
 
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
-
-function Address(street, city, zipCode){
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode;
-}
-
-function areEqual(address1, address2){
-    return address1.street === address2.street &&
-        address1.city === address2.city &&
-        address1.zipCode === address2.zipCode;
-}
-
-function areSame(address1, address2){
-    return address1 === address2;
-}
+console.log(post);
