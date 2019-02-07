8618add7630c8230641f7e1d64c0e631056e86a0
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-07 23:59:01 +0100

    Arrays - mapping

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,7 +1,26 @@
 const numbers = [1, -1, 2, 3];
 
-// const filtered = numbers.filter(function(value) {
-//     return value >= 0;
+
+// const items = filtered.map(n => '<li>' + n + '</li>');
+
+// const html = '<ul>' + items.join('') + '</ul>';
+
+// const items = filtered.map(n => {
+    //     const obj = {value: n};
+//     return obj;
 // });
 
-const filtered = numbers.filter(n => n >= 0);
+// const items = filtered.map(n => {
+    //     return {value: n};
+// });
+
+// const filtered = numbers.filter(n => n >= 0);
+// const items = filtered.map(n => ({value: n}));
+
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({value: n}))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+
+console.log(items);
