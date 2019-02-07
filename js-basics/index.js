2f043dcab8f37b06c2201a05a82d3ebd7b10e675
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-07 18:26:12 +0100

    Arrays - finding elements-primitives

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,7 +1,8 @@
-const numbers = [3, 4];
+const numbers = [1, 2, 3, 1, 4];
 
-numbers.push(5,6);
-numbers.unshift(1,2);
-numbers.splice(2,0,'a','b');
+console.log(numbers.indexOf('a'));
+console.log(numbers.indexOf(1, 2));
+console.log(numbers.lastIndexOf(1));
 
-console.log(numbers);
+console.log(numbers.indexOf(1) !== -1);
+console.log(numbers.includes(1));
