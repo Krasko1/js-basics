42b9ac0ae49149284156d450c46119074aeabe58
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-07 22:56:06 +0100

    Arrays - the spread operator

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,9 +1,8 @@
 const first = [1,2,3];
-const first = [{id:1}];
 const second = [4,5,6];
 
-const combined = first.concat(second);
+// const combined = first.concat(second);
+const combined = [...first, 'a', ...second, 'b'];
 
-const slice = combined.slice(2,4);
-const slice = combined.slice(2);
-const slice = combined.slice();
+// const copy = combined.slice();
+const copy = [...combined];
