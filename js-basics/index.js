961be0224c09def3be3fcd7b5368311ae293aee5
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-07 23:44:47 +0100

    Arrays - filtering

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,9 +1,7 @@
 const numbers = [1, -1, 2, 3];
 
-const allPositive = numbers.every(function(value){
-    return value >= 0;
-});
+// const filtered = numbers.filter(function(value) {
+//     return value >= 0;
+// });
 
-const atLeastOnePositive = numbers.some(function(value){
-    return value >= 0;
-});
+const filtered = numbers.filter(n => n >= 0);
