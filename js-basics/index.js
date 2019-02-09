9faa2afc6c0ad1752246e6c89e7b47ce9ac95c25
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-09 19:55:30 +0100

    Arrays - Ex4 moving an element

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,14 +1,18 @@
 const numbers = [1, 2, 3, 4];
 
-const output = except(numbers, [1, 2]);
+const output = move(numbers, 0, 1);
 
 console.log(output);
 
-function except(array, excluded) {
-    const output = [];
+function move(array, index, offset) {
+    const position = index + offset;
+    if(position >= array.length || position < 0){
+        console.error('Invalid offset.');
+        return;
+    }
 
-    for(let element of array)
-        if(!excluded.includes(element))
-            output.push(element);
-    return output;
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(position, 0, element);
+    return output; 
 }
