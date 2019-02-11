8c2073a696e1a2d59e3bf5b345a077ba71e86924
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-11 10:01:25 +0100

    Functions - the rest operator

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,16 +1,19 @@
-function sum(){
-    let total = 0;
-    for(let value of arguments)
-        total += value;
-    return total;
-}
-
-// function sum(a, b){
-     // return a + b;
+// function sum(...args){
+//     return args.reduce((a, b) => a + b);
 // }
 
-// console.log(sum(1, 2));
-// console.log(sum(1));
-// console.log(sum());
-console.log(sum(1, 2, 3, 4, 5));
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum(discount, ...prices){
+    const total = prices.reduce((a, b) => a + b);
+    return total * (1 - discount);
+}
+
+console.log(sum(0.1, 20, 30));
+
+// NO:
+// function sum(discount, ...prices, value){
+//     const total = prices.reduce((a, b) => a + b);
+//     return total * (1 - discount);
+// }
 
