ca2e27d92866d94f45207134d3a9c8947d00bdcb
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-11 22:52:04 +0100

    Functions - try and catch

--- js-basics/index.js
+++ js-basics/index.js
@@ -5,12 +5,24 @@ const person = {
         return `${person.firstName} ${person.lastName}`;
     },
     set fullname(value){
+        if(typeof value !== 'string')
+            throw new Error('Value is not a string');
+        
         const parts = value.split(' ');
+
+        if(parts.length !== 2)
+            throw new Error('Enter a first name and last name');
+
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 };
 
-person.fullname = 'Iboja Tot-Djerdj';
+try{
+    person.fullname = '';
+}
+catch(e){
+    alert(e);
+}
 
 console.log(person);
