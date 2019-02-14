6b9e315d4be862c7f7ad54528163d5c4bd3607a5
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-14 22:47:32 +0100

    Ex2 - Area of circle

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,9 +1,8 @@
-function sum(...items){
-    if(items.length === 1 && Array.isArray(items[0]))
-        items = [...items[0]];
+const circle = {
+    radius: 1,
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
 
-    return items.reduce((a, b) => a + b);
-}
-
-console.log(sum(1, 2, 3, 4));
-console.log(sum([1, 2, 3, 4]));
+console.log(circle.area);
